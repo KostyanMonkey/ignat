71aca24aecf04274fa42fb1e294c31fac1fd75c1
KostyanMonkey <114530003+KostyanMonkey@users.noreply.github.com>
2024-02-03 15:53:42 +0300

    Add files via upload

---
+++ zxc.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+
+struct User
+{
+    std::string name;
+    int age;
+};
+
+std::vector<User> users;
+
+void create()
+{
+    User x;
+    std::cin >> x.name;
+    std::cin >> x.age;
+    users.push_back(x);
+}
+
+void del()
+{
+    std::string namedel;
+    std::cin >> namedel;
+    for (int i = 0; i < users.size(); i++)
+    {
+        if (namedel == users[i].name)
+        {
+            users.erase(users.begin() + i);
+        }
+    }
+}
+
+void update()
+{
+    std::string nameupdate;
+    std::cin >> nameupdate;
+    for (int i = 0; i < users.size(); i++)
+    {
+        if (nameupdate == users[i].name)
+        {
+            std::cin >> users[i].age;
+           
+        }
+    }
+}
+
+void print()
+{
+    for (int i = 0; i < users.size(); i++)
+    {
+        std::cout << "username: " <<  users[i].name << " age: " << users[i].age << "\n";
+    }
+}
+
+int main()
+{
+    void (*makesmth)();
+    makesmth = create;
+    makesmth();
+    makesmth = print;
+    makesmth();
+    makesmth = update;
+    makesmth();
+    makesmth = print;
+    makesmth();
+    makesmth = del;
+    makesmth();
+    makesmth = print;
+    makesmth();
+    
+
+}
+
